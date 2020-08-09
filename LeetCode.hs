3dbcc6586d745c07f70152c756ec5de7c3328f39
Ninad Jog <NinadJog@users.noreply.github.com>
2020-08-09 16:14:03 -0400

    Update LeetCode.hs
    Added a few comments for Problem 1. 
    Renamed findNum function to findNumIndex. 
    Also added comments about two potential alternative solutions for Problem 1

--- LeetCode.hs
+++ LeetCode.hs
@@ -1,3 +1,8 @@
+{- 
+  Updated: August 9, 2020
+  Ninad Jog
+-}
+
 module LeetCode where
 import Data.Char
   
@@ -6,31 +11,106 @@ import Data.Char
 
 -- Problem 1
 {-
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+https://leetcode.com/problems/two-sum/
 
-Assume that each input would have exactly one solution, and you may not use the same element twice.
+Given an array of integers, return indices of the two numbers such that they add up to a specific target. Assume that each input would 
+have exactly one solution, and you may not use the same 
+element twice.
+
+Example: Given nums = [2, 7, 11, 15], target = 9,
+return [0, 1] because nums[0] + nums[1] = 2 + 7 = 9
+
 -}
 
-twoSum :: [Int] -> Int -> Int -> [Int]
-twoSum [] _ _ = []
-twoSum (x:xs) target idx1 
-  | idx2 /= -1  = [idx1, idx1 + idx2 + 1]
-  | otherwise   = twoSum xs target (idx1 + 1)
+---------
+{-
+Solution #1.
+
+This is the main function. It calls a helper function, twoSum',
+starting at index 0. twoSum' in turn calls a function named
+findNumIndex.
+
+Example:
+twoSum [2, -7, 11, 15] 8 returns [1, 3] because -7 + 15 = 8
+
+If the no two elements from the input list add up to the target
+number, this function returns an empty list. For example,
+
+twoSum [2, -7, 11, 15] 20
+returns [] because no two numbers from the list add up to 20.
+-}
+
+twoSum :: [Int] -> Int -> [Int]
+twoSum xs target = twoSum' xs target 0
+
+---------
+{-
+This function does the actual work. It begins by taking the 
+first number from the list and tries to find a number 
+that equals targetSum MINUS firstNumber in the remainder of
+the list (so that firstNum + that number = targetSum)
+
+If findNum returns a non-negative number, it means such a
+number has been found 
+If such a number is not found (i.e. if findNum returns -1), 
+it moves on to the next number in the list.
+-}
+
+twoSum' :: [Int] -> Int -> Int -> [Int]
+twoSum' [] _ _ = []
+twoSum' (x:xs) target idx1 
+  | idx2 /= -1  = [idx1, idx1 + idx2 + 1]      -- base case
+  | otherwise   = twoSum' xs target (idx1 + 1) -- recursive case
   where
-    idx2 = findNum xs (target - x) 0
+    idx2 = findNumIndex xs (target - x) 0
 
 ----------
--- Find the index (position) of the given number in the given list
+{-
+Find the index (position) of the given number in the given list
+Return -1 if the number is not found. Instead of returning
+-1 if the number is not found, we could have changed the return
+type of this function to Maybe Int and returned None.
 
-findNum :: [Int] -> Int -> Int -> Int
-findNum [] _ _ = -1     -- If partner is not found
-findNum (x:xs) num pos
+Examples:
+findNum [0, 1, 2, 3, 4] 3 0 
+returns 3 because the index of 3 in the list is 3
+
+findNum [5, 4, -3] 2 0
+returns -1 becuse 2 does not appear in the list
+-}
+
+findNumIndex :: [Int] -> Int -> Int -> Int
+findNumIndex [] _ _ = -1     -- If number is not found
+findNumIndex (x:xs) num pos
   | x == num = pos
-  | otherwise = findNum xs num (pos+1)
+  | otherwise = findNumIndex xs num (pos+1)
   
----------------------------
+-----------------------------
+{-
+Solution #2
+Another strategy (not explored here) would be to zip the
+input list with [0...] so that each element in the list
+will have its index paired with it.
+
+The code will probably be simpler with that strategy
+-}
+
+-----------------------------
+{-
+Solution #3
+See whether we can use list a list comprehension
+-}
+
+-- This method is not currently used. Just exploring.
+pairs :: Int -> [(Int, Int)]
+pairs n = [(a, b) | a <- [0..n-1], b <- [a+1..n-1], a+b == 3]
+
+
+-- ==========================================================
 {-
 Problem 2.
+
+https://leetcode.com/problems/add-two-numbers/
 
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
@@ -69,14 +149,15 @@ encodeNum (x:xs) = (encodeNum xs) ++ [(ord x) - 48]
 
 -- =======================================================
 {-
-
 Problem 6.
+
+https://leetcode.com/problems/zigzag-conversion/
 
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
-P   A   H   N
-A P L S I I G
-Y   I   R
+P     A       H       N
+ A  P   L   S   I   I   G
+  Y       I       R
 And then read line by line: "PAHNAPLSIIGYIR"
 
 ----
@@ -86,10 +167,10 @@ Input: s = "PAYPALISHIRING", numRows = 4
 Output: "PINALSIGYAHRPI"
 Explanation:
 
-P     I    N
-A   L S  I G
-Y A   H R
-P     I
+P           I           N
+  A       L   S       I   G
+    Y   A       H   R
+      P           I
 
 -}
 
