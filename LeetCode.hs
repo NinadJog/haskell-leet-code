3cf220aba9872d5e6e47e6c802cbdb180afaf400
Ninad Jog <NinadJog@users.noreply.github.com>
2020-08-11 14:32:49 -0400

    Update LeetCode.hs
    Swapped positions of solution #2 and #1 for Problem 1. Added more comments

--- LeetCode.hs
+++ LeetCode.hs
@@ -1,5 +1,5 @@
 {- 
-  Updated: August 9, 2020
+  Updated: August 11, 2020
   Ninad Jog
 -}
 
@@ -20,13 +20,86 @@ element twice.
 Example: Given nums = [2, 7, 11, 15], target = 9,
 return [0, 1] because nums[0] + nums[1] = 2 + 7 = 9
 
+There are 2 solutions: one obtained from zipping up the numbers
+with their indices and the other without. A solution using list
+comprehension might be a third one, but I haven't come up with it 
+yet.
 -}
 
 ---------
 {-
-Solution #1.
+Solution #1
 
-This is the main function. It calls a helper function, twoSum',
+Whenever we have to work with the positions of elements in 
+a Haskell list (a.k.a. array indices), it's best to pair each
+element with its index. This can be done by zipping the list
+with [0..]
+
+That's what the twoSumZip function does before passing on the
+paired list to twoSumZip'
+-}
+
+------------
+{-
+Pair the input list with integers starting from 0.
+Example:
+
+Input:  [5, 4, -2]
+Output: [(5, 0), (4, 1), (-2, 2)]
+
+-}
+twoSumZip :: [Int] -> Int -> [Int]
+twoSumZip xs num = twoSumZip' ys num
+  where 
+    ys = zip xs [0..]
+
+------------
+{-
+Takes the first number in the list and tries to find
+a second number from the rest of the list that adds up
+to the target. Calls the findNumIdx function to find the
+index of such a number. findNumIdx returns -1 if such
+a number is not found.
+
+If such a number is not found, recursively move on to 
+the next number in the list and repeat the search.
+-}
+
+twoSumZip' :: [(Int, Int)] -> Int -> [Int]
+twoSumZip' [] _ = []
+twoSumZip' ((x, idx1):xs) target  
+  | idx2 /= -1  = [idx1, idx2]        -- base case
+  | otherwise   = twoSumZip' xs target -- recursive case
+  where
+    idx2 = findNumIdx xs (target - x)
+    
+------------
+{-
+Given a number and a list of tuples containing numbers 
+paired up with their indices, this function returns the index
+of the number if the number is found, otherwise returns -1.
+
+Example:
+Inputs: [(5, 0), (4, 1), (-2, 2)] (-2)
+Output: 2
+-}
+
+findNumIdx :: [(Int, Int)] -> Int -> Int
+findNumIdx [] _ = -1
+findNumIdx ((x, idx):xs) num
+  | x == num  = idx
+  | otherwise = findNumIdx xs num
+  
+
+-----------------------------
+{-
+Solution #2.
+
+This was my first attempt at a solution before I thought of
+solution #1. It should be just as performant as #1, but is
+probably not as elegant.
+
+twoSum is the main function. It calls a helper function, twoSum',
 starting at index 0. twoSum' in turn calls a function named
 findNumIndex.
 
@@ -87,23 +160,16 @@ findNumIndex (x:xs) num pos
   
 -----------------------------
 {-
-Solution #2
-Another strategy (not explored here) would be to zip the
-input list with [0...] so that each element in the list
-will have its index paired with it.
-
-The code will probably be simpler with that strategy
--}
-
------------------------------
-{-
 Solution #3
-See whether we can use list a list comprehension
+See whether we can use a list comprehension
 -}
 
 -- This method is not currently used. Just exploring.
 pairs :: Int -> [(Int, Int)]
-pairs n = [(a, b) | a <- [0..n-1], b <- [a+1..n-1], a+b == 3]
+pairs n = [(a, b) | a <- [0..n-1], b <- [a+1..n-1]]
+
+sums :: [Int] -> Int -> [(Int, Int)]
+sums ys@(x:xs) num = [(a, b) | a <- ys, b <- xs, a+b == num]
 
 
 -- ==========================================================
